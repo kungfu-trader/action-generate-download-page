12a1be35d6831f300b2cb1b6812ac70cd72466ac
xcvdsfd <xcvdsfd@gmail.com>
2023-12-12 16:43:56 +0800

    [update] add generateHTML method

---
+++ index.js
@@ -0,0 +1,25 @@
+const lib = (exports.lib = require("./lib"));
+const core = require("@actions/core");
+const github = require("@actions/github");
+
+const main = async function () {
+  const context = github.context;
+  const argv = {
+    apiKey: core.getInput("apiKey"),
+    bucketRelease: core.getInput("kungfu-releases"),
+    bucketPrebuilt: core.getInput("kungfu-prebuilt"),
+    baseId: core.getInput("airtable-baseid"),
+    owner: context.payload.repository.owner.login,
+    repo: context.payload.repository.name,
+    pullRequestTitle: context.payload?.pull_request?.title,
+  };
+  lib.generateHTML(argv);
+};
+
+if (require.main === module) {
+  main().catch((error) => {
+    console.error(error);
+    // 设置操作失败时退出
+    core.setFailed(error.message);
+  });
+}
